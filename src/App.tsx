ba0023052f4740ec7705b71f5ffdc8897fff397c
georgekvr <george.kvrivi@gmail.com>
2023-08-23 00:05:00 +0400

    chore: add prettier and set it up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import SrcAlias from "@/components/SrcAlias"
 
 const App = ()=>  {
-
   return (
     <>
-    <div className="text-xl text-red-500 font-bold">Something cool is gonna be born soon...</div>
-    <SrcAlias/>
+      <div className="text-xl text-red-500 font-bold">Something cool is gonna be born soon...</div>
     </>
   )
 }
