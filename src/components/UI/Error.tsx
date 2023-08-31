ca59e5961c9f1c63f57966af49b2f6d0255fe39b
georgekvr <george.kvrivi@gmail.com>
2023-09-01 02:18:33 +0400

    refactor: change searchbar color on dark mode

--- src/components/UI/Error.tsx
+++ src/components/UI/Error.tsx
@@ -3,7 +3,7 @@ import ErrorWarrningIcon from '@/components/icons/ErrorWarrningIcon';
 const Error = ({ message }: { message: string | undefined }) => {
    if (message) {
       return (
-         <p className="flex gap-2  text-red-400 font-bold break-words relative">
+         <p className="flex gap-2 mt-[2px] text-red-400 font-bold break-words relative">
             <ErrorWarrningIcon />
             <span className="ml-6">{message}</span>
          </p>
--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -7,12 +7,12 @@ interface InputProps {
 
 const Input = (props: InputProps) => {
    return (
-      <div className="w-full flex items-center py-2 px-3 bg-[#f5f5f5] rounded-full shadow-input">
+      <div className="w-full flex items-center py-2 px-3 bg-[#f5f5f5] dark:sm:bg-[#161616] rounded-full shadow-input">
          {props.children}
          <input
             type="text"
             placeholder={props.placeholder}
-            className="w-full px-2 outline-none bg-inherit placeholder-[#6b6b6b] text-gray-700"
+            className="w-full px-2 outline-none bg-inherit placeholder-[#6b6b6b] text-gray-700 dark:text-white"
          />
       </div>
    );
